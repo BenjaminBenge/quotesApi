4fe620100bd15c87c0e6851c5f8168a30bd82e49
Benjamin Benge <bengebenjamin@gmail.com>
2025-04-15 18:06:04 +0300

    Added HTML quote app

---
+++ quotes.js
@@ -0,0 +1,15 @@
+// const quote = document.getElementById("myquote");
+
+
+
+const api_url ="https://zenquotes.io/api/quotes/";
+
+async function getapi(url)
+{
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+}
+
+getapi(api_url);
+

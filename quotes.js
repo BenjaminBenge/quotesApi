16c67f86ab83d3d2fa5734dcfb1a9d6620381772
Benjamin Benge <bengebenjamin@gmail.com>
2025-04-16 16:37:08 +0300

    made the quotes API work

--- quotes.js
+++ quotes.js
@@ -4,12 +4,13 @@
 
 const api_url ="https://zenquotes.io/api/quotes/";
 
-async function getapi(url)
+async function getapi()
 {
-  const response = await fetch(url);
+  const response = await fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes");
   var data = await response.json();
   console.log(data);
+  document.getElementById("quote").textContent = data || "Couldn't find a joke!";
 }
 
-getapi(api_url);
+getapi();
 
